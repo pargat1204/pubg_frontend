4b499d163e18b1790b1da35c7fdc4b5b1d707329
unknown <pargat161187120@gmail.com>
2023-11-10 13:33:50 +0530

    volume-05

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Header.scss";
 import { BiLogIn } from "react-icons/bi";
 import { FaUserCircle } from "react-icons/fa";
 import { Link, NavLink, useNavigate } from "react-router-dom";
@@ -25,77 +26,41 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="header-bottom skewBg" data-header="">
-        <div className="container">
-          <div className="logo" onClick={goHome}>
-            <BiLogIn size={35} />
-            <span>Gamics</span>
-          </div>
-          <nav className="navbar" data-navbar="">
-            <ul className="navbar-list">
-              <li className="navbar-item">
-                <a href="/" className="navbar-link skewBg" data-nav-link="">
-                  Home
-                </a>
-              </li>
-              <li className="navbar-item">
-                <a href="#blog" className="navbar-link skewBg" data-nav-link="">
-                  Blog
-                </a>
-              </li>
-              <li className="navbar-item">
-                <a href="#" className="navbar-link skewBg" data-nav-link="">
-                  Contact
-                </a>
-              </li>
-              <ShowOnLogin>
-                <li className="navbar-item">
-                  <FaUserCircle size={20} />
-                  <UserName />
-                </li>
-              </ShowOnLogin>
-              <ShowOnLogout>
-                <li>
-                  <button className="navbar-item">
-                    <Link to="/login">Login</Link>
-                  </button>
-                </li>
-              </ShowOnLogout>
-              <ShowOnLogin>
-                <li>
-                  <NavLink to="/profile" className={activeLink}>
-                    Profile
-                  </NavLink>
-                </li>
-                <li>
-                  <button onClick={logoutUser} className="navbar-item">
-                    Logout
-                  </button>
-                </li>
-              </ShowOnLogin>
-            </ul>
-          </nav>
-          <div className="header-actions">
-            <button
-              className="search-btn"
-              aria-label="open search"
-              data-search-toggler=""
-            >
-              <ion-icon name="search-outline" />
-            </button>
-            <button
-              className="nav-toggle-btn"
-              aria-label="toggle menu"
-              data-nav-toggler=""
-            >
-              <ion-icon name="menu-outline" className="menu" />
-              <ion-icon name="close-outline" className="close" />
-            </button>
-          </div>
+      <nav>
+        <div className="logo" onClick={goHome}>
+          <BiLogIn size={35} />
+          <span>AUTH:Z</span>
         </div>
-      </div>
-    </header>
 
+        <ul className="home-links">
+          <ShowOnLogin>
+            <li className="--flex-center">
+              <FaUserCircle size={20} />
+              <UserName />
+            </li>
+          </ShowOnLogin>
+          <ShowOnLogout>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/login">Login</Link>
+              </button>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogin>
+            <li>
+              <NavLink to="/profile" className={activeLink}>
+                Profile
+              </NavLink>
+            </li>
+            <li>
+              <button onClick={logoutUser} className="--btn --btn-secondary">
+                Logout
+              </button>
+            </li>
+          </ShowOnLogin>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
